3f8f12150adabbabff88944f060bd76c99c72671
Cristian Aldea <aldea.cristian@outlook.com>
2019-12-02 10:53:41 -0500

    Update custom bash profile with aliases and PROMPT_COMMAND

--- .custom-bash-profile.sh
+++ .custom-bash-profile.sh
@@ -49,6 +49,21 @@ function parse_git_dirty {
 
 export PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[31m\]\`parse_git_branch\`\[\e[m\]\\$ "
 
+
+############ CUSTOM TERMINAL TAB NAME ###########
+
+export PROMPT_COMMAND='echo -en "\033]0;$(whoami)@$(hostname):$(dirs +0)\a"'
+
 ############# CUSTOM ALIASES ###############
 
+# my-config
+alias my-config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
+
+
+# ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Misc
 alias fucking="sudo"
