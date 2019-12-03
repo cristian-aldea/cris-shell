d9c0ae21b0e0007d6da05c41dce1ce4f273a98b9
Cristian Aldea <aldea.cristian@outlook.com>
2019-12-02 23:01:08 -0500

    Add zsh, p10k, and git config

---
+++ .shell_aliases.sh
@@ -0,0 +1,13 @@
+############# CUSTOM ALIASES ###############
+
+# my-config
+alias my-config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
+
+
+# ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Misc
+alias fucking="sudo"
