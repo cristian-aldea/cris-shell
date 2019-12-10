851b6cbc9ffc2c716355b1443f5735411c4459f2
Cristian Aldea <aldea.cristian@outlook.com>
2019-12-09 21:53:52 -0500

    Add git aliases

--- .custom.sh
+++ .custom.sh
@@ -4,10 +4,15 @@
 alias my-config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
 
 
-# ls aliases
+# Aliases
+
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-# Misc
 alias fucking="sudo"
+
+# Git Aliases
+git config --global alias.sync '!git pull; git push'
+git config --global alias.co checkout
+git config --global alias.unstage 'reset HEAD --'
