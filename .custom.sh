4c74d0ab4cce11d710dd7e1edbb247b1e6e59eb4
Cristian Aldea <aldea.cristian@outlook.com>
2021-11-02 18:24:37 -0400

    move git config to file instead of commands
    no need to rerun the command everytime a new shell is started, the .gitconfig file can be used

--- .custom.sh
+++ .custom.sh
@@ -4,7 +4,7 @@
 
 ############### ALIASES ###############
 
-# lconfig
+# myconfig
 # see https://github.com/cristian-aldea/bash-me
 alias myconfig='git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
 
@@ -18,29 +18,6 @@ alias python="python3"
 alias pip="pip3"
 alias k=kubectl
 alias mkdir='mkdir -p'
-
-############### GIT CONFIG ###############
-git config --global pager.branch false
-
-# Git aliases
-git config --global alias.unstage 'reset HEAD --'
-git config --global alias.current-branch 'rev-parse --abbrev-ref HEAD'
-git config --global alias.publish '!git push -u origin $(git current-branch)'
-
-git config --global alias.co checkout
-git config --global alias.s status
-
-git config --global alias.rlb 'branch -m'
-git config --global alias.rrb '!f() { git push origin --delete "$1" && git push origin -u "$2"; }; f'
-git config --global alias.rb '!f() { git rlb "$2" && git rrb "$1" "$2"; }; f'
-git config --global alias.nuke '!git reset --hard && git clean -df'
-git config --global alias.sync '!git pull --rebase --prune && git push'
-git config --global alias.ac '!git add -A && git commit'
-git config --global alias.cp '!git commit && git push'
-git config --global alias.acp '!git add -A && git commit && git push'
-git config --global alias.uc 'reset --soft HEAD~1'
-git config --global alias.fp 'push --force-with-lease'
-git config --global alias.prune-branches '!git branch  | grep -v \* | xargs git branch -D'
 
 ############### ENVIRONMENT VARIABLES ###############
 
